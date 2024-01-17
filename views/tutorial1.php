d5c82667448f670119c0d5516bdee43c32b1cf2f
MayMyatNoeOo <may.myatnoeoo@seattleconsultingmyanmar.com>
2024-01-17 16:03:36 +0630

    [add] first tutorial

---
+++ views/tutorial1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <h3> Testing PHP Syntax</h3>
+  </head>
+  <body>
+    <div>
+      <h4> Tutorial 1</h4>
+      <?php echo "First Tutorial"
+      ?>
+
+    </div>
+
+  </body>
+  <footer>
+
+  </footer>
+</html>
